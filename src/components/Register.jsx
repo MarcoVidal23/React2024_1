8578a97de5c0415169171051b6b1a8f2d4be7693
Marco Vidal <marcovidal_27@hotmail.com>
2024-08-14 00:25:33 -0400

    "Nuevo Commit"

---
+++ src/components/Register.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+const Register = () => {
+  //Estados del formulario
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [edad, setEdad] = useState("");
+  const [email, setEmail] = useState("");
+  //Estado para los errores
+  const [error, setError] = useState(false);
+  //Función antes de enviar el formulario
+  const validarDatos = (e) => {
+    e.preventDefault();
+    //Validación;
+    if (!nombre.trim() || !apellido.trim() || !edad.trim() || !email.trim()) {
+      setError(true);
+      return;
+    }
+    setError(false);
+  };
+  return (
+    <>
+      <hr /> <h1>Registro de Usuario</h1>
+      <form className="formulario" onSubmit={validarDatos}>
+        {error ? alert("Todos los campos son obligatorios") : null}
+        <div className="form-group">
+                    <label>Nombre</label>         {" "}
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+          />
+        </div>
+        <div className="form-group">
+          <label>Apellido</label>
+          <input
+            type="text"
+            name="apellido"
+            className="form-control"
+            onChange={(e) => setApellido(e.target.value)}
+            value={apellido}
+          />
+        </div>
+        <div className="form-group">
+          <label>Edad</label>
+          <input
+            type="text"
+            name="edad"
+            className="form-control"
+            onChange={(e) => setEdad(e.target.value)}
+            value={edad}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Enviar
+        </button>
+      </form>
+      <hr />
+    </>
+  );
+};
+export default Register;

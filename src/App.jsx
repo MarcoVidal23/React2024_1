8dc405c59665f0e3dcefade46977e978a5cd05f2
Marco Vidal <marcovidal_27@hotmail.com>
2024-08-19 11:55:05 -0400

    Nuevo Commit 1908

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 
 function App() {
   const data = [
@@ -30,10 +31,9 @@ function App() {
 
   return (
     <>
-     
-   
-      
-     <Footer />
+      <Navbar />
+
+      <Footer />
     </>
   );
 }

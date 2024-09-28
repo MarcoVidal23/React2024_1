49477db83de965ac36f81f2573aa2ab753f59450
Marco Vidal <marcovidal_27@hotmail.com>
2024-09-28 19:55:39 -0300

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState,useEffect } from 'react';
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
-import CardPizza from "./components/CardPizza";
-import { pizza } from "./data/pizza";
+import LoginPage from "./components/LoginPage";
+import Register from "./components/Register";
 
 
 function App() {
-const initialCart = () => {
-  const localStorageCart = localStorage.getItem("cart");
-  return localStorageCart ? JSON.parse(localStorageCart) : [];
-};
-
-  const [dato, setDato] = useState(pizza); 
- const [cart, setCart] = useState(initialCart);
-const MIN_ITEMS = 1;
-const MAX_ITEMS = 5;
-
-useEffect(() => {
-  localStorage.setItem("cart", JSON.stringify(cart));}, [cart]);  
-
-  function addToCart(item) {
-    const itemExists = cart.findIndex((carro) => carro.id === item.id)
-    if (itemExists >= 0) {
-      const updatedCart = [...cart]
-      updatedCart[itemExists].cantidad++
-      setCart(updatedCart)
-    } else {
-      item.cantidad =1
-      setCart([...cart, item])
-    }
-      
-  }
-  function removeFromCart(id) {
-    setCart((prevCart) => prevCart.filter((carro) => carro.id !== id));
-  }
-
-  function decreaseQuantity(id) {
-    const updatedCart = cart.map((item) => {
-      if (item.id === id && item.cantidad > 0 ) {
-        return {
-          ...item,
-          cantidad: item.cantidad - 1,
-        };
-      }
-      return item;
-    });
-    setCart(updatedCart);
-  }
-
-  function increaseQuantity(id) {
-    const updatedCart = cart.map((item) => {
-      if (item.id === id && item.cantidad >= 0) {
-        return {
-          ...item,
-          cantidad: item.cantidad + 1,
-        };
-      }
-      return item;
-    });
-    setCart(updatedCart);
-  }
-
-  function clearCart(e) {
-    setCart([]);
-  }
 
 
     
   return (
     <>
-      <Navbar
-        cart={cart}
-        removeFromCart={removeFromCart}
-        decreaseQuantity={decreaseQuantity}
-        increaseQuantity={increaseQuantity}
-        clearCart={clearCart}
-      />
-
-      <Header />
-
-      <div className="grid-pizza">
-        {dato.map((card) => (
-          <CardPizza key={card.id} card={card} setCart={setCart} addToCart={addToCart} />
-        ))}
-      </div>
+    
 
     
+
+      <LoginPage />
+
+      <Register />
+
       <Footer />
     </>
   );
